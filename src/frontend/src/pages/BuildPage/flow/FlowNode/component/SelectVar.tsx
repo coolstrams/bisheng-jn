4f9cbd95d6a676e61c18fbc5f08cab6da8f1427b
coolstrams <caikai890617@gmail.com>
2025-02-12 13:53:49 +0800

    0.4.1.2放开file类型表单变量

--- src/frontend/src/pages/BuildPage/flow/FlowNode/component/SelectVar.tsx
+++ src/frontend/src/pages/BuildPage/flow/FlowNode/component/SelectVar.tsx
@@ -7,9 +7,9 @@ import NodeLogo from "../NodeLogo";
 
 const isMatch = (obj, expression) => {
     // 临时关闭 file类型表单变量
-    if (obj.key === 'form_input') {
-        expression = "value.filter(el => el.type !== 'file').map(el => ({ label: el.key, value: el.key }))"
-    }
+    // if (obj.key === 'form_input') {
+    //     expression = "value.filter(el => el.type !== 'file').map(el => ({ label: el.key, value: el.key }))"
+    // }
     const fn = new Function('value', `return ${expression}`);
     return fn(obj.value);
 };
